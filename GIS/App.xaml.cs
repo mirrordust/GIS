fd7697b34e0949ced0c86e1809269d6778edb1a3
mirrordust <liaoshanhe@gmail.com>
2016-06-26 12:59:46 +0800

    initial

---
+++ GIS/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using ESRI.ArcGIS;
+using ESRI.ArcGIS.esriSystem;
+
+namespace GIS
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitArcGISLicence();
+        }
+
+        public void InitArcGISLicence()
+        {
+            RuntimeManager.Bind(ProductCode.EngineOrDesktop);
+
+            AoInitialize aoi = new AoInitialize();
+            esriLicenseProductCode productCode = esriLicenseProductCode.esriLicenseProductCodeAdvanced;
+            if (aoi.IsProductCodeAvailable(productCode) == esriLicenseStatus.esriLicenseAvailable)
+            {
+                aoi.Initialize(productCode);
+            }
+        }
+    }
+}
---
+++ GIS/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geometry;
+
+namespace GIS
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AxMapControl m_map = new AxMapControl();
+        AxTOCControl m_toc = new AxTOCControl();
+        AxToolbarControl m_toolbar = new AxToolbarControl();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.mapControlHost.Child = m_map;
+            this.tocHost.Child = m_toc;
+            this.toolbarHost.Child = m_toolbar;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            IMapDocument mapDoc = new MapDocument();
+            mapDoc.Open(Properties.Resources.ArcMapRelativePath);
+            m_map.Map = mapDoc.get_Map(0);
+            m_toc.SetBuddyControl(m_map);
+            m_toolbar.SetBuddyControl(m_map);
+            m_toolbar.AddItem("esriControls.ControlsMapNavigationToolbar");
+        }
+    }
+}
